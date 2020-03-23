4e2e6335f012d4cb9084ed8a3f66c732c3991ce3
Wentao Ding <wtding1998@gmail.com>
2020-03-23 16:35:46 +0800

    add LSTM

--- train_stnn.py
+++ train_stnn.py
@@ -27,7 +27,7 @@ import torch.backends.cudnn as cudnn
 
 from get_dataset import get_stnn_data
 from utils import DotDict, Logger, rmse, boolean_string, get_dir, get_time, time_dir
-from stnn import SaptioTemporalNN, SaptioTemporalNN_largedecoder, SaptioTemporalNN_GRU
+from stnn import SaptioTemporalNN, SaptioTemporalNN_largedecoder, SaptioTemporalNN_GRU, SaptioTemporalNN_LSTM
 
 def train(command=False):
     if command == True:
@@ -200,8 +200,11 @@ def train(command=False):
     if opt.model == 'default':
         model = SaptioTemporalNN(relations, opt.nx, opt.nt_train, opt.nd, opt.nz, opt.mode, opt.nhid, opt.nlayers,
                             opt.dropout_f, opt.dropout_d, opt.activation, opt.periode).to(device)
-    if opt.model == 'GRU':
+    elif opt.model == 'GRU':
         model = SaptioTemporalNN_GRU(relations, opt.nx, opt.nt_train, opt.nd, opt.nz, opt.mode, opt.nhid, opt.nlayers,
+                            opt.dropout_f, opt.dropout_d, opt.activation, opt.periode).to(device)
+    elif opt.model == 'LSTM':
+        model = SaptioTemporalNN_LSTM(relations, opt.nx, opt.nt_train, opt.nd, opt.nz, opt.mode, opt.nhid, opt.nlayers,
                             opt.dropout_f, opt.dropout_d, opt.activation, opt.periode).to(device)
     elif opt.model == 'ld':
         model = SaptioTemporalNN_largedecoder(relations, opt.nx, opt.nt_train, opt.nd, opt.nz, opt.mode, opt.nhid, opt.nlayers, opt.nhid_de, opt.nlayers_de, opt.dropout_de,
