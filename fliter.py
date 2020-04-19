b0816251eff9c5f1887fd43eb06665b259417bdc
phyyj <phyyj@login1.pi.sjtu.edu.cn>
2020-04-19 10:47:24 +0800

    fix error in variable'sname

--- fliter.py
+++ fliter.py
@@ -3,20 +3,19 @@ import shutil
 
 source_dir = '../output'
 target_dir = '../output_result'
-outputdirs = ['algorithm']
+outputdirs = ['japan', 'feb0416', 'jar0416', 'mar0416']
 
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
-
 for dir_name in outputdirs:
     outputdir = os.path.join(source_dir, dir_name)
-    target_dir = os.path.join(target_dir, dir_name)
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
+    targetdir = os.path.join(target_dir, dir_name)
+    if not os.path.exists(targetdir):
+        os.makedirs(targetdir)
     xps = os.listdir(outputdir)
     for xp in xps:
         xp_path = os.path.join(outputdir, xp)
-        target_path = os.path.join(target_dir, xp)
+        target_path = os.path.join(targetdir, xp)
         if not os.path.exists(target_path):
             os.makedirs(target_path)
         file_names = os.listdir(xp_path)
