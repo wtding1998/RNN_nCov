ea19c55953f43e3c399950f220e89865589be4f0
Wentao Ding <wtding1998@gmail.com>
2020-05-04 21:58:07 +0800

    choose true model

--- fliter.py
+++ fliter.py
@@ -4,7 +4,7 @@ from result import *
 
 source_dir = '../output'
 target_dir = '../output_result'
-outputdirs = ['japan']
+outputdirs = ['test_fli']
 
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
@@ -16,12 +16,16 @@ for dir_name in outputdirs:
     xps = os.listdir(outputdir)
     for xp in xps:
         xp_path = os.path.join(outputdir, xp)
-        exp = Exp(xp, outputdir)
-        target_path = os.path.join(targetdir, xp)
-        if not os.path.exists(target_path):
-            os.makedirs(target_path)
         file_names = os.listdir(xp_path)
-        for file_name in file_names:
-            if file_name == 'config.json' or '.txt' in file_name:
-                source_file = os.path.join(xp_path, file_name)
-                shutil.copy(source_file, target_path)
+        if 'model.pt' in file_names:
+            print(xp)
+            exp = Exp(xp, outputdir)
+            target_path = os.path.join(targetdir, xp)
+            if not os.path.exists(target_path):
+                os.makedirs(target_path)
+            for file_name in file_names:
+                if file_name == 'config.json' or '.txt' in file_name:
+                    source_file = os.path.join(xp_path, file_name)
+                    shutil.copy(source_file, target_path)
+        else:
+            print(xp, 'x')
